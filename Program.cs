d137a450723f27283a60eb224a1c2b42aff68ad3
elpisofthelake <acharyasachin429@gmail.com>
2024-02-06 20:09:35 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 50; i < 70; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
